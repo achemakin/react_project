e48c568ff136e33963ed6e40499716cb8b6b8ba5
achemakin <kotor79@gmail.com>
2021-10-09 21:34:00 +0300

    simply with webpack-dev-server

---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import styles from './header.css';
+
+export function Header() {
+  return (
+    <header>
+      <h1 className={styles.example}>Reddit for our own 111</h1>
+    </header>
+  )
+}
