904e99c496c876c2737a79db1c40f2c6801229c4
achemakin <kotor79@gmail.com>
2021-10-09 23:39:28 +0300

    ssr

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,7 @@
-const path = require('path');
-const HTMLWebpacPlugin = require('html-webpack-plugin');
+const clientConfig = require('./cfg/webpack.client.config');
+const serverConfig = require('./cfg/webpack.server.convig');
 
-const NODE_ENV = process.env.NODE_ENV;
-const IS_DEV = NODE_ENV === 'development';
-const IS_PROD = NODE_ENV === 'production';
-
-function setupDevtool() {
-  if (IS_DEV) return 'eval';
-  if (IS_PROD) return false;
-}
-
-module.exports = {
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
-  },
-  mode: NODE_ENV ? NODE_ENV : 'development',
-  entry: path.resolve(__dirname, 'src/index.jsx'),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.[tj]sx?$/,
-        use: ['ts-loader']
-      }, 
-      {
-        test: /\.css$/, //\.less$
-        use: [
-          'style-loader', {
-            loader: 'css-loader',
-            options: {
-              modules: {
-                mode: 'local',
-                localIdentName: '[name]__[local]--[hash:base64:5]',
-              }              
-            }
-          },
-          //'less-loader',
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new HTMLWebpacPlugin({
-      template: path.resolve(__dirname, 'index.html'),
-      inject: 'head',
-      scriptLoading: 'defer'
-    })
-  ],
-  devServer: {
-    port: 3000,
-    open: true,
-    hot: IS_DEV,
-  },
-  devtool: setupDevtool()
-}
+module.exports = [
+  clientConfig,
+  serverConfig
+];
